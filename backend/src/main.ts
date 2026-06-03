6c3958a1f8da087e41450c55865cdcbb2ac60686
RoGoLo-05 <robertogomezlopez2005@gmail.com>
2026-06-03 16:54:28 +0200

    fix: open CORS for debugging

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
   app.enableCors({
-    origin: ['http://localhost:3001', 'https://chatflow-woad.vercel.app'],
+    origin: true,
     credentials: true,
   });
 
